8a9d9414d38e5a210fe4adf133b94202b4f43a56
Narathip Thakham <narathip.tk@gmail.com>
2026-08-20 17:45:55 +0700

    add provider warper for shadcn

--- src/providers.jsx
+++ src/providers.jsx
@@ -2,11 +2,17 @@ import { RouterProvider } from "react-router";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { router } from "./router";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "@/components/ui/toast";
 
 export default function Providers() {
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <Toaster>
+        <TooltipProvider>
+          <RouterProvider router={router} />
+        </TooltipProvider>
+      </Toaster>
     </QueryClientProvider>
   );
 }
